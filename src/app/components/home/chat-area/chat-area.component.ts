dfc5da6f1bb8dfcc4f14fda090e71f81a4177543
boris-szl <boris.szelcsanyi@outlook.com>
2022-12-17 11:27:59 +0100

    chore: design chat bubbles

--- src/app/components/home/chat-area/chat-area.component.ts
+++ src/app/components/home/chat-area/chat-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { AfterViewChecked, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { Subscription } from "rxjs";
 import { ChatService } from "../../../shared/services/chat.service";
 import { ChatMessage } from "src/app/shared/models/message";
@@ -10,16 +10,17 @@ import { ChatMessage } from "src/app/shared/models/message";
     styleUrls: ['./chat-area.component.css']
 })
 
-export class ChatArea implements OnInit, OnDestroy {
+export class ChatArea implements OnInit, OnDestroy, AfterViewChecked {
     
     @Input() nickname = '';
-    
+
     public errorMessage = '';
     private chatService$?: Subscription;
     public messages: ChatMessage[] = [];
 
-
     @ViewChild('scrollFrame') private scrollFrame!: ElementRef<HTMLElement>;
+
+    @ViewChild('scrollMe') private myScrollContainer!: ElementRef;
 
     constructor(private chatService: ChatService) {}
 
@@ -31,11 +32,17 @@ export class ChatArea implements OnInit, OnDestroy {
     ngOnInit(): void {
         this.getHistory();
         this.scrollTo();
+        this.scrollToBottom();
 
         setInterval(() => {
             this.getHistory();
-        }, 2500);
+        }, 1000);
     }
+
+    ngAfterViewChecked() {        
+        this.scrollToBottom();        
+    }
+    
 
     public isItMe(nickname : string) : boolean {
         return this.nickname?.toLowerCase() === nickname?.toLocaleLowerCase();
@@ -59,5 +66,11 @@ export class ChatArea implements OnInit, OnDestroy {
             left: 0,
             behavior: 'smooth',
         });
+    }
+
+    scrollToBottom(): void {
+        try {
+            this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+        } catch(err) { }                 
     }
 }
